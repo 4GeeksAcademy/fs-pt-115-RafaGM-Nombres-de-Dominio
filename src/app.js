49c44f75835f3e961d22935f69ed9e26bd474f71
Rafa-progz <rafa.trabajo@outlook.es>
2025-06-23 10:59:16 +0000

    mejorandolo

--- src/app.js
+++ src/app.js
@@ -5,32 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
 
-   const pronoun = ['the', 'our'];
-   const adj = ['great', 'big'];
-   const noun = ['jogger', 'racoon'];
-    for (let i = 0 ; i < pronoun.length ; i++){
-      for (let j = 0; j < adj.length; j++){
-        for (let k = 0; k < noun.length; k++){
-          const dominio = pronoun[i]+adj[j]+noun[k]+".com";
-          console.log(dominio);
-        }
+  const pronoun = ['the', 'our'];
+  const adj = ['great', 'big'];
+  const noun = ['jogger', 'racoon'];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        const dominio = pronoun[i] + adj[j] + noun[k] + ".com";
+        console.log(dominio);
       }
     }
-   const pronouns = ['the', 'our'];
-   const adjs = ['great', 'big'];
-   const nouns = ['jogger', 'racoon'];
-   const dominios = []
-      pronouns.map(pronombre => {
-        adjs.map(adjetivo => {
-          nouns.map(nombre => {
-           dominios.push(pronombre + adjetivo + nombre + '.com')
-       })
-     })
-   })
-   console.log(dominios);
+  }
+  const pronouns = ['the', 'our'];
+  const adjs = ['great', 'big'];
+  const nouns = ['jogger', 'racoon'];
+  const dominios = []
+  pronouns.map(pronombre => {
+    adjs.map(adjetivo => {
+      nouns.map(nombre => {
+        dominios.push(pronombre + adjetivo + nombre + '.com')
+      })
+    })
+  })
+  console.log(dominios);
+  for (let i = 0; i < dominios.length; i++){
+   console.log(dominios[i]);
+    
+  }
 };
 
