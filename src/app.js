8146e1f02735154a7f628819dd9a51a0fc8a3d69
Rafa-progz <rafa.trabajo@outlook.es>
2025-06-23 09:54:12 +0000

    dominios

--- src/app.js
+++ src/app.js
@@ -8,4 +8,29 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+   const pronoun = ['the', 'our'];
+   const adj = ['great', 'big'];
+   const noun = ['jogger', 'racoon'];
+    for (let i = 0 ; i < pronoun.length ; i++){
+      for (let j = 0; j < adj.length; j++){
+        for (let k = 0; k < noun.length; k++){
+          const dominio = pronoun[i]+adj[j]+noun[k]+".com";
+          console.log(dominio);
+        }
+      }
+    }
+   const pronouns = ['the', 'our'];
+   const adjs = ['great', 'big'];
+   const nouns = ['jogger', 'racoon'];
+   const dominios = []
+      pronouns.map(pronombre => {
+        adjs.map(adjetivo => {
+          nouns.map(nombre => {
+           dominios.push(pronombre + adjetivo + nombre + '.com')
+       })
+     })
+   })
+   console.log(dominios);
 };
+
